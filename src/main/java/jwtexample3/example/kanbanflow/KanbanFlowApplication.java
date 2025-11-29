36a482d0951b254a29ab90e76cb5cb642cae8fed
Aniket Khangar <aniketkhangar459@gmail.com>
2025-11-29 19:39:06 +0530

    First Commit

---
+++ src/main/java/jwtexample3/example/kanbanflow/KanbanFlowApplication.java
@@ -0,0 +1,13 @@
+package jwtexample3.example.kanbanflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jwtexample3/example/kanbanflow/KanbanFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package jwtexample3.example.kanbanflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
