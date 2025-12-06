56332e8d2f030126f07255d6d7feac9fa33a69f9
Aniket Khangar <aniketkhangar459@gmail.com>
2025-12-06 20:32:33 +0530

    removed issue

--- src/main/java/jwtexample3/example/kanbanflow/repository/UserRepository.java
+++ src/main/java/jwtexample3/example/kanbanflow/repository/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
-   List<User> findByEmail(String email);
-   List<User> finByPhone(String phone);
 
 }
